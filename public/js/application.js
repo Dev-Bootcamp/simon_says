6e014b5fe91065eccd68d4b2e135a6723847cc9b
Geoff Crittenden <geoffcritt@hotmail.com>
2013-09-26 14:07:06 -0500

    mock assessment

---
+++ public/js/application.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.container').on('click','#get_color', function (e) {
+    e.preventDefault();
+    $.post('/color', function(response) {
+      $('ul li:nth-child(' + response.cell + ')').css('background-color', response.color);
+    });
+  });
+});
